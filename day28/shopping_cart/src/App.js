b9d85fb21547ac42b40e243321d5feaf2868dc8e
Bharathan <bharathan655@gmail.com>
2023-11-05 13:27:08 +0530

    day28 Shopping cart using Context Task Completed

--- day28/shopping_cart/src/App.js
+++ day28/shopping_cart/src/App.js
@@ -1,10 +1,24 @@
-import Cart from "./Components/Cart";
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CartProvider from './Context/CartContext'; 
+import Navbar from './Components/Navbar';
+import ProductList from './Components/ProductList';
+import CartPage from './Components/CartPage';
 
 function App() {
-return(
-  <Cart/>
-)
+  return (
+    <BrowserRouter>
+      <div>
+        <CartProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </CartProvider>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
