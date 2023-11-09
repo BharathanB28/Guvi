ad1426a7df014df00e4b82287e0e39efc7b3be10
Bharathan <bharathan655@gmail.com>
2023-11-09 20:54:17 +0530

    day 26 Task completed

--- day26/react_router_task/src/App.js
+++ day26/react_router_task/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './Components/Navigation';
-import CourseContent from './Components/CourseContent';
 import './App.css';
-import courses from './Components/CoursesData';
+import All from './Components/All'
+import FullStack from './Components/FullStack';
+import DataScience from './Components/DataScience';
+import CyberSecurity from './Components/CyberSecurity';
+import Career from './Components/Career';
 
 
 const App = () => {
   return (
     <Router>
-      <Navigation courses={courses} />
-      <Routes>
-        
-        <Route path="/" element={<CourseContent course={courses[0]} />} />
-
-        {courses.map((course) => (
-          <Route key={course.id} path={`/courses/${course.id}`} element={<CourseContent course={course} />} />
-        ))}
-      </Routes>
+      <Navigation/>
+        <Routes>
+          <Route index path="/" element={<All />} />
+          <Route path="/courses/fullstack" element={<FullStack />} />
+          <Route path="/courses/datascience" element={<DataScience />} />
+          <Route path="/courses/cybersecurity" element={<CyberSecurity />} />
+          <Route path="/courses/career" element={<Career />} />        
+        </Routes>
     </Router>
   );
 };
