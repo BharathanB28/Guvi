e60cc0b0a6eefd6a661756aea5b916988c529826
Bharathan <bharathan655@gmail.com>
2023-11-13 00:18:55 +0530

    day15 Task Re_Submitted

--- day15/DOM Manipulation with Forms/script.js
+++ day15/DOM Manipulation with Forms/script.js
@@ -6,9 +6,20 @@ dataForm.addEventListener('submit', function (element) {element.preventDefault()
     const firstName = document.getElementById('first-name').value
     const lastName = document.getElementById('last-name').value
     const address = document.getElementById('address').value
-    const pincode = document.getElementById('pincode').value
+    const pincodeRegex = /^\d{6}$/;
+      const pincode = document.getElementById('pincode').value;
+
+      if (!pincodeRegex.test(pincode)) {
+        alert('Please enter a valid 6-digit pin code.');
+        return;
+      }
     const gender = document.querySelector('input[name="gender"]:checked').value
-    const foodArray = Array.from(document.querySelectorAll('input[name="food"]:checked')).map(checkbox => checkbox.value)
+    const foodArray = Array.from(document.querySelectorAll('input[name="food"]:checked')).map(checkbox => checkbox.value);
+
+      if (foodArray.length < 2) {
+        alert('Please select at least two food items.');
+        return;
+      }
     const state = document.getElementById('state').value
     const country = document.getElementById('country').value
 
