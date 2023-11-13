47cf5d4b1c8d8fc5b48a8ce5792c3ebecfbf416a
Bharathan <bharathan655@gmail.com>
2023-11-13 14:13:32 +0530

    day 20 Task Re_Submitted

--- day20/API-1/script.js
+++ day20/API-1/script.js
@@ -1,24 +1,46 @@
-const loadStoreAPI = () =>{
-    fetch("https://fakestoreapi.com/products")
-    .then(responce => responce.json())
-    .then(data => displayProducts(data))
-}
+const loadStoreAPI = async () => {
+    try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        displayProducts(data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+};
 
-//Get data
-const getProduct =(product)=>{
-    return (`
-    <div class="product">
-        <img src="${product.image}">
-        <h2>${product.title}</h2>
-        <h2>${product.category}</h2>
-        <h2>${product.price}</h2>
-    </div>
-    `)
-}
-//Dispaly Data
-const displayProducts = (products) =>{
-const displayHTML = products.map(product=>getProduct(product))
-const container = document.getElementById("products")
-container.innerHTML = displayHTML.join(" ")
-}
+const createProductElement = (product) => {
+    const productElement = document.createElement("div");
+    productElement.classList.add("product");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = product.image;
+    imgElement.alt = product.title;
+
+    const titleElement = document.createElement("h2");
+    titleElement.textContent = product.title;
+
+    const categoryElement = document.createElement("p");
+    categoryElement.textContent = product.category;
+
+    const priceElement = document.createElement("p");
+    priceElement.textContent = `$${product.price.toFixed(2)}`; // Format price
+
+    productElement.appendChild(imgElement);
+    productElement.appendChild(titleElement);
+    productElement.appendChild(categoryElement);
+    productElement.appendChild(priceElement);
+
+    return productElement;
+};
+
+const displayProducts = (products) => {
+    const container = document.getElementById("products");
+    container.innerHTML = ""; // Clear existing content
+
+    products.forEach(product => {
+        const productElement = createProductElement(product);
+        container.appendChild(productElement);
+    });
+};
+
 loadStoreAPI();
--- day20/API-2/script.js
+++ day20/API-2/script.js
@@ -1,25 +1,82 @@
-// Fetch data from the REST Countries API to populate the country cards
 fetch('https://restcountries.com/v3.1/all')
-.then(response => response.json())
-.then(data => {
-    const countryCards = document.getElementById('country-cards')
-
-    data.forEach(country => {
-        const card = document.createElement('div')
-        card.classList.add('col-lg-4', 'col-md-6', 'col-sm-12')
-        card.innerHTML = `
-            <div class="card">
-                <div class="card-header text-white bg-black">
-                        <h5>${country.name.common}</h5>
-                    </div>
-                <div class="card-body bg-info" ; color: white;">
-                    <img div.row div.col-sm-6.col-md-4.col-lg-4.col-xl-4 div.card img.card-img-top src="${country.flags.png}" style="width: 100%; height: 180px;">                               
-                    <p class="card-text">Capital: ${country.capital}</p>
-                    <p class="card-text">Region: ${country.region}</p>
-                    <p class="card-text">Country Code: ${country.cca3}</p>
-                </div>
-            </div>
-        `
-        countryCards.appendChild(card)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
     })
-})
+    .then(data => {
+        const countryCards = document.getElementById('country-cards');
+        const fragment = document.createDocumentFragment();
+
+        data.forEach(country => {
+            const card = createCountryCard(country);
+            fragment.appendChild(card);
+        });
+
+        countryCards.appendChild(fragment);
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
+
+function createCountryCard(country) {
+    const card = document.createElement('div');
+    card.classList.add('col-lg-4', 'col-md-6', 'col-sm-12');
+
+    const cardInner = document.createElement('div');
+    cardInner.classList.add('card');
+
+    const cardHeader = createCardHeader(country.name.common);
+
+    const cardBody = createCardBody(country);
+
+    cardInner.appendChild(cardHeader);
+    cardInner.appendChild(cardBody);
+
+    card.appendChild(cardInner);
+
+    return card;
+}
+
+function createCardHeader(countryName) {
+    const cardHeader = document.createElement('div');
+    cardHeader.classList.add('card-header', 'text-white', 'bg-black');
+
+    const countryNameElement = document.createElement('h5');
+    countryNameElement.textContent = countryName;
+
+    cardHeader.appendChild(countryNameElement);
+
+    return cardHeader;
+}
+
+function createCardBody(country) {
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body', 'bg-info');
+
+    const flagImage = document.createElement('img');
+    flagImage.classList.add('card-img-top');
+    flagImage.src = country.flags.png;
+    flagImage.alt = `${country.name.common} Flag`;
+    flagImage.style.width = '100%';
+    flagImage.style.height = '180px';
+
+    const capitalText = createCardText(`Capital: ${country.capital}`);
+    const regionText = createCardText(`Region: ${country.region}`);
+    const countryCodeText = createCardText(`Country Code: ${country.cca3}`);
+
+    cardBody.appendChild(flagImage);
+    cardBody.appendChild(capitalText);
+    cardBody.appendChild(regionText);
+    cardBody.appendChild(countryCodeText);
+
+    return cardBody;
+}
+
+function createCardText(textContent) {
+    const cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    cardText.textContent = textContent;
+    return cardText;
+}
